3a00f1a1c1b5ce1a3d35803a7f6bd6d7bc616698
Dan Fyfe <fyfedaniel@gmail.com>
2024-03-21 10:56:40 -0400

    basic post creation without db

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { IPost } from './interfaces';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
   @Get()
-  getPosts(): { title: string; content: string } {
+  getPosts(): IPost[] {
     return this.postsService.allPosts();
+  }
+
+  @Post()
+  addPost(
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ): IPost[] {
+    return this.postsService.insertPost(title, content);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { IPost } from './interfaces';
+import { Post } from './post.model';
 
 @Injectable()
 export class PostsService {
-  allPosts(): { title: string; content: string } {
-    return {
-      title: 'title',
-      content: 'content',
-    };
+  posts: IPost[] = [
+    {
+      title: 'title 1',
+      content: 'content 1',
+    },
+    {
+      title: 'title 2',
+      content: 'content 2',
+    },
+  ];
+
+  allPosts(): IPost[] {
+    return this.posts;
+  }
+
+  insertPost(title: string, content: string): IPost[] {
+    // console.log('title: ', title)
+    const newPost: IPost = new Post(title, content);
+    this.posts.push(newPost);
+    return this.posts;
   }
 }
