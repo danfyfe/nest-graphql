9bec3c20b6eae892fa533bc6e5fe7132dc1168e6
Dan Fyfe <fyfedaniel@gmail.com>
2024-03-21 11:19:30 -0400

    add basic all posts test

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
+import { IPost } from './interfaces';
+import { PostsModule } from './posts.module';
 
 describe('PostsService', () => {
   let service: PostsService;
@@ -7,6 +9,7 @@ describe('PostsService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PostsService],
+      imports: [PostsModule]
     }).compile();
 
     service = module.get<PostsService>(PostsService);
@@ -14,5 +17,22 @@ describe('PostsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('allPosts', () => {
+    it('should return all posts', () => {
+      const results: IPost[] = [
+        {
+          title: 'title 1',
+          content: 'content 1',
+        },
+        {
+          title: 'title 2',
+          content: 'content 2',
+        },
+      ];
+      const allPostResults = service.allPosts();
+      expect(allPostResults).toStrictEqual(results);
+    });
   });
 });
